2f30ebc345a16b469c5ef57b7e5b8813d806b7f6
quangng2000 <quang.ngn2000@gmail.com>
2023-08-24 16:10:37 -0500

    initial commit

--- src/main/java/com/example/demo/controller/SparkController.java
+++ src/main/java/com/example/demo/controller/SparkController.java
@@ -1,46 +1,46 @@
-package com.example.demo.controller;
-
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.dto.GeoDto;
-import com.example.demo.dto.TaxAuctionDto;
-import com.example.demo.service.SparkService;
-import com.example.demo.service.WebScrappingService;
-
-@RestController
-public class SparkController {
-
-	@Autowired
-	SparkService service;
-	
-	@Autowired
-	WebScrappingService webService;
-
-	
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/api")
-	public List<TaxAuctionDto> sqlController() {
-        
-		
-
-        return service.sparkService();
-    }
-	@RequestMapping(method = RequestMethod.GET, path = "/extract")
-	public String extractController() {
-        
-		
-
-        return webService.webScrapping();
-    }
-	
-	
-	
-
-}
+//package com.example.demo.controller;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.example.demo.dto.GeoDto;
+//import com.example.demo.dto.TaxAuctionDto;
+//import com.example.demo.service.SparkService;
+//import com.example.demo.service.WebScrappingService;
+//
+//@RestController
+//public class SparkController {
+//
+//	@Autowired
+//	SparkService service;
+//	
+//	@Autowired
+//	WebScrappingService webService;
+//
+//	
+//	
+//	@RequestMapping(method = RequestMethod.GET, path = "/api")
+//	public List<TaxAuctionDto> sqlController() {
+//        
+//		
+//
+//        return service.sparkService();
+//    }
+//	@RequestMapping(method = RequestMethod.GET, path = "/extract")
+//	public String extractController() {
+//        
+//		
+//
+//        return webService.webScrapping();
+//    }
+//	
+//	
+//	
+//
+//}
--- src/main/java/com/example/demo/service/WebScrappingService.java
+++ src/main/java/com/example/demo/service/WebScrappingService.java
@@ -1,34 +1,34 @@
-package com.example.demo.service;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import com.example.demo.dto.GeoDto;
-
-@Service
-public class WebScrappingService {
-	
-	
-	public String webScrapping() {
-		RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = "https://nominatim.openstreetmap.org/search?format=json&q={address}";
-        String address =formatAddress("11001 E MOUNT VERNON CT WICHITA KS 67207-7712");
-        GeoDto[] obj = restTemplate.getForObject(apiUrl, GeoDto[].class, address);
-		
-		
-		
-		
-		return obj[0].getLat();
-	}
-	
-	public String formatAddress(String originalAddress) {
-        int lastSpaceIndex = originalAddress.lastIndexOf(" ");
-        if (lastSpaceIndex != -1) {
-            String formattedAddress = originalAddress.substring(0, lastSpaceIndex);
-            return formattedAddress;
-        } else {
-            return originalAddress; // Return original if no space found
-        }
-    }
-	
-}
+//package com.example.demo.service;
+//
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//import com.example.demo.dto.GeoDto;
+//
+//@Service
+//public class WebScrappingService {
+//	
+//	
+//	public String webScrapping() {
+//		RestTemplate restTemplate = new RestTemplate();
+//        String apiUrl = "https://nominatim.openstreetmap.org/search?format=json&q={address}";
+//        String address =formatAddress("11001 E MOUNT VERNON CT WICHITA KS 67207-7712");
+//        GeoDto[] obj = restTemplate.getForObject(apiUrl, GeoDto[].class, address);
+//		
+//		
+//		
+//		
+//		return obj[0].getLat();
+//	}
+//	
+//	public String formatAddress(String originalAddress) {
+//        int lastSpaceIndex = originalAddress.lastIndexOf(" ");
+//        if (lastSpaceIndex != -1) {
+//            String formattedAddress = originalAddress.substring(0, lastSpaceIndex);
+//            return formattedAddress;
+//        } else {
+//            return originalAddress; // Return original if no space found
+//        }
+//    }
+//	
+//}
